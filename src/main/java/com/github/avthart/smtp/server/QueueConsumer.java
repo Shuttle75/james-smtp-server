561cadc62b77ddba94f49373b391154278ba4990
Oleksii Osadchyi <oleksii.osadchyi@iohk.io>
2023-03-25 12:53:19 +0200

    Add MimeMessageParser

--- src/main/java/com/github/avthart/smtp/server/QueueConsumer.java
+++ src/main/java/com/github/avthart/smtp/server/QueueConsumer.java
@@ -1,11 +1,18 @@
 package com.github.avthart.smtp.server;
 
 import org.apache.activemq.broker.BrokerService;
+import org.apache.commons.mail.util.MimeMessageParser;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Service;
 
 import javax.jms.JMSException;
 import javax.jms.ObjectMessage;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.internet.MimeMessage;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Properties;
 
 
 @Service
@@ -18,8 +25,22 @@ public class QueueConsumer {
     }
 
     @JmsListener(destination = "smtp.queue")
-    public void listener(ObjectMessage objectMessage) throws JMSException {
-        objectMessage.getObject();
+    public void listener(ObjectMessage objectMessage) throws JMSException, MessagingException {
+        InputStream inputStream = new ByteArrayInputStream((byte[]) objectMessage.getObject());
+
+        Properties props = System.getProperties();
+        Session mailSession = Session.getInstance(props);
+        MimeMessage mimeMessage = new MimeMessage(mailSession, inputStream);
+
+        try {
+            final MimeMessageParser mimeParser = new MimeMessageParser(mimeMessage).parse();
+            System.out.println("Attachments " + mimeParser.getAttachmentList().size());
+        }
+        catch (Exception e) {
+            // Error parsing
+            throw new RuntimeException(e);
+        }
+
         producerBroker.checkQueueSize("smtp.queue");
     }
 }
